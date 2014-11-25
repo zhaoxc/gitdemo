c2172bc3c4133591ee346990f9117804d6aa7b90
Administrator <Administrator@zhaoxc-cb471753>
2014-11-25 23:46:27 +0800

    ddd

--- gittest/src/Mytest.java
+++ gittest/src/Mytest.java
@@ -6,7 +6,7 @@ public class Mytest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello world");
+		System.out.print("hello asssworld");
 
 	}
 
